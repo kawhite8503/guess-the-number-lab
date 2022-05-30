e7b797e30b228e1a3b13a3dc0fe7023b8525d6e5
Kathy White <kawhite8503@gmail.com>
2022-05-30 15:37:15 -0500

    add to play function

--- app.js
+++ app.js
@@ -6,16 +6,21 @@ const game = {
   prevGuesses: []
   getGuess: function(guess){
     prompt(`Enter a guess between ${smallestNum} and ${biggestNum}`)
-    const parsed = parseInt(guess)
-    if (isNaN(parsed)) {
-      return NaN
-    } else if (guess <= this.biggestNum && guess >= this.smallestNum){
-      return parsed
+    const parsedGuess = parseInt(guess)
+    while (parsedGuess <= this.biggestNum && parsedGuess >= this.smallestNum) {
+      return parsedGuess
     }
+    if (isNaN(parsedGuess)) {
+      return NaN
     }
   }
   play: function() {
     this.secretNum = Math.floor(Math.random() * 
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+      getGuess()
+      while (getGuess !== this.secretNum) {
+        prevGuess.push(getGuess())
+      }
   }
+  
 }
