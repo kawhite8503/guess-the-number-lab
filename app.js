32ddb41356c6159dd2a26100c72c27e3b21cf535
Kathy White <kawhite8503@gmail.com>
2022-05-30 18:33:03 -0500

    final touches before level up

--- app.js
+++ app.js
@@ -3,33 +3,33 @@ const game = {
   biggestNum: 100,
   smallestNum: 1,
   secretNum: null,
-  prevGuesses: []
-  getGuess: function(guess){
-    prompt(`Enter a guess between ${smallestNum} and ${biggestNum}`)
-    const parsedGuess = parseInt(guess)
-    while (parsedGuess <= this.biggestNum && parsedGuess >= this.smallestNum) {
-      return parsedGuess
-    }
-    if (isNaN(parsedGuess)) {
-      return NaN
-    }
-  }
+  prevGuesses: [],
   play: function() {
     this.secretNum = Math.floor(Math.random() * 
-      (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
-      getGuess()
-      while (getGuess !== this.secretNum) {
-        prevGuess.push(getGuess())
-      }
-      render()
-  }
+    (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+    do {
+      this.prevGuesses.push(this.getGuess())
+      this.render()
+    } while (this.prevGuesses[this.prevGuesses.length - 1] !== this.secretNum) 
+  },
+  getGuess: function(){
+    let parsedGuess
+    while(!parsedGuess || parsedGuess > this.biggestNum || parsedGuess < this.smallestNum) {
+      parsedGuess = parseInt(prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}`))
+      if (isNaN(parsedGuess) || parsedGuess > this.biggestNum || parsedGuess < this.smallestNum) {
+        alert('This is not a valid entry')
+    }}
+    return parsedGuess
+  },
   render: function() {
-    if (this.getGuess === this.secretNum) {
+    if (this.secretNum === this.prevGuesses[this.prevGuesses.length - 1]) {
       alert(`Congrats! You guessed the number in ${this.prevGuesses.length} guesses`)
-    } else if (getGuess > this.secretNum) {
+    } else if (this.secretNum < this.prevGuesses[this.prevGuesses.length - 1]) {
       alert(`Your guess is too high. Previous guesses ${this.prevGuesses.join(', ')}`)
-    } else if (getGuess < this.secretNum) {
+    } else if (this.secretNum > this.prevGuesses[this.prevGuesses.length - 1]) {
       alert(`Your guess is too low. Previous guesses ${this.prevGuesses.join(', ')}`)
     }
   }
 }
+
+game.play()
